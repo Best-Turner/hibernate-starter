c5793929bcd6773e1fc1eebe48b8823529939c23
Aleksandr Trofimov <shurikx6881@gmail.com>
2025-06-30 23:00:54 +0300

    start practic

--- src/main/java/com/demdev/HibernateRunner.java
+++ src/main/java/com/demdev/HibernateRunner.java
@@ -1,7 +1,35 @@
 package com.demdev;
 
+import com.demdev.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
+
 public class HibernateRunner {
     public static void main(String[] args) {
 
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);
+        configuration.configure();
+
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("petr@mail.ru")
+                    .firstname("Petr")
+                    .lastname("Petrov")
+                    .birthDay(LocalDate.of(2010, 5, 21))
+                    .age(15)
+                    .build();
+
+            session.save(user);
+            session.getTransaction().commit();
+        }
     }
 }
---
+++ src/main/java/com/demdev/entity/User.java
@@ -0,0 +1,32 @@
+package com.demdev.entity;
+
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+    private String firstname;
+    private String lastname;
+    @Column(name = "birth_day")
+    private LocalDate birthDay;
+    private Integer age;
+}
